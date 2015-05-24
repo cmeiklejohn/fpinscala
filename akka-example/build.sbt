0db94348dba181d4704e3725c3d6ed22fd93637e
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2015-05-23 23:59:56 -0400

    Further the example.

--- akka-example/build.sbt
+++ akka-example/build.sbt
@@ -8,3 +8,6 @@ libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.4-SNAPSHOT"
 
 resolvers += "Akka Snapshot Repository" at "http://repo.akka.io/snapshots/"
 
+scalacOptions += "-deprecation"
+
+scalacOptions += "-feature"
--- akka-example/src/main/scala/Greeting.scala
+++ akka-example/src/main/scala/Greeting.scala
@@ -3,6 +3,10 @@ import akka.actor.Props
 import akka.event.Logging
 import akka.actor.ActorSystem
 
+import akka.actor.OneForOneStrategy
+import akka.actor.SupervisorStrategy._
+import scala.concurrent.duration._
+
 case object Happy
 case object Angry
 case class Greeting(who: String)
@@ -23,13 +27,27 @@ class GreetingActor extends Actor {
   }
 }
 
+class GreetingSupervisor extends Actor {
+  override val supervisorStrategy =
+    OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 1.minute) {
+      case _: ArithmeticException  => Resume
+      case _: NullPointerException => Restart
+      case _: Exception            => Escalate
+    }
+
+    def receive = {
+      case p: Props => sender() ! context.actorOf(p)
+    }
+}
+
 object GreetingApplication extends App {
   val system = ActorSystem("GreetingSystem")
+  val supervisor = system.actorOf(Props[GreetingSupervisor], "supervisor")
   val greeter = system.actorOf(Props[GreetingActor], "greeter")
 
   greeter ! Greeting("Charlie Parker")
   greeter ! Angry
   greeter ! Greeting("Amelie Poulin")
 
-  system.shutdown
+  system.terminate
 }
